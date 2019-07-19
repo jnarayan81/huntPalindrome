d077f24e0da087861ceaa2a905365becbbd181a4
Jitendra Narayan <jnarayan81@gmail.com>
2019-07-19 05:35:00 +0530

    Update reformatAln.pl

--- scriptBase/reformatAln.pl
+++ scriptBase/reformatAln.pl
@@ -4,6 +4,7 @@ use strict;
 use warnings;
 
 my $infile=$ARGV[0];
+my $mode=$ARGV[1];
 
 open my $fh, '<', "$infile" or die "Cannot open file: $!\n";
 
@@ -15,10 +16,15 @@ while(<$fh>) {
     #if ( -z 'all.combined' ) { print "File is empty!\n"; exit;}
     my @tmpLine = split '\t', $line;
     #print "$line\t";
-    print "$tmpLine[0]\t$tmpLine[1]\t$tmpLine[2]\t$tmpLine[3]\t$tmpLine[4]\t$tmpLine[6]\t$tmpLine[8]\t";
+    #print "$tmpLine[0]\t$tmpLine[1]\t$tmpLine[2]\t$tmpLine[3]\t$tmpLine[4]\t$tmpLine[6]\t$tmpLine[8]\t";
     my $nextLine = <$fh>; chomp $nextLine;
     my @nLine = split '\t', $nextLine;
     my $orientation="NA";
     if ($nLine[6] eq $tmpLine[6]) { $orientation="direct_repeat"; } else { $orientation="inverted_repeat"; }
-    if ($nLine[8] == $tmpLine[8]) { print "$nLine[0]\t$nLine[3]\t$nLine[4]\t$nLine[6]\t$nLine[8]\t$orientation\n";}
+
+    if (($nLine[8] == $tmpLine[8]) && ($mode eq "inverted")) { next if $orientation eq "direct_repeat";
+        print "$tmpLine[0]\t$tmpLine[1]\t$tmpLine[2]\t$tmpLine[3]\t$tmpLine[4]\t$tmpLine[6]\t$tmpLine[8]\t$nLine[0]\t$nLine[3]\t$nLine[4]\t$nLine[6]\t$nLine[8]\t$orientation\n";}
+    elsif (($nLine[8] == $tmpLine[8]) && ($mode eq "direct")) { next if $orientation eq "inverted_repeat";
+         print "$tmpLine[0]\t$tmpLine[1]\t$tmpLine[2]\t$tmpLine[3]\t$tmpLine[4]\t$tmpLine[6]\t$tmpLine[8]\t$nLine[0]\t$nLine[3]\t$nLine[4]\t$nLine[6]\t$nLine[8]\t$orientation\n";}
+    else { print "$tmpLine[0]\t$tmpLine[1]\t$tmpLine[2]\t$tmpLine[3]\t$tmpLine[4]\t$tmpLine[6]\t$tmpLine[8]\t$nLine[0]\t$nLine[3]\t$nLine[4]\t$nLine[6]\t$nLine[8]\t$orientation\n";}
 }
